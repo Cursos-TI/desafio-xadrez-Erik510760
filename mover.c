72a8add808a9996b4bf8ba15efb95535d8c23a14
Erik510760 <erikryanoliveira6@gmail.com>
2025-05-13 18:11:19 -0300

    Create mover.c

---
+++ mover.c
@@ -0,0 +1,20 @@
+def mover(tabuleiro, origem, destino):
+    x1, y1 = origem
+    x2, y2 = destino
+    peca = tabuleiro[x1][y1]
+
+    if peca is None:
+        print("Não há peça na posição de origem.")
+        return False
+
+    movimentos = peca.movimentos_possiveis(tabuleiro, x1, y1)
+
+    if (x2, y2) not in movimentos:
+        print("Movimento inválido.")
+        return False
+
+    imprimir_direcoes(origem, destino)
+
+    tabuleiro[x2][y2] = peca
+    tabuleiro[x1][y1] = None
+    return True
