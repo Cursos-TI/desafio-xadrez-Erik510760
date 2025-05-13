d7b545086b333bf66966bc6f0710f5f7bab8aa4f
Erik510760 <erikryanoliveira6@gmail.com>
2025-05-13 17:57:17 -0300

    Create lógica para mover as peças.c

---
+++ lógica para mover as peças.c
@@ -0,0 +1,13 @@
+def mover(tabuleiro, origem, destino):
+    x1, y1 = origem
+    x2, y2 = destino
+    peca = tabuleiro[x1][y1]
+    if peca is None:
+        print("Não há peça na posição de origem.")
+        return False
+    if (x2, y2) not in peca.movimentos_possiveis(tabuleiro, x1, y1):
+        print("Movimento inválido.")
+        return False
+    tabuleiro[x2][y2] = peca
+    tabuleiro[x1][y1] = None
+    return True
