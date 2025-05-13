d487f4f4aa8367e1691628e7c5794853e1e04cce
Erik510760 <erikryanoliveira6@gmail.com>
2025-05-13 18:13:03 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,9 @@
+mover(tabuleiro, (6, 0), (4, 0))
+  printf("Cima\n");
+printf("Cima\n");
+mover(tabuleiro, (7, 2), (4, 5))
+  printf("Cima Direita\n");
+printf("Cima Direita\n");
+printf("Cima Direita\n");
+
+
