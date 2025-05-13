1ccf222b4dd9c4552f1097a1a039e1bfad949910
Erik510760 <erikryanoliveira6@gmail.com>
2025-05-13 18:04:23 -0300

    Create Classe_rainha.c

---
+++ Classe_rainha.c
@@ -0,0 +1,20 @@
+class Rainha(Peca):
+    def movimentos_possiveis(self, tabuleiro, x, y):
+        movimentos = []
+        direcoes = [
+            (-1, 0), (1, 0), (0, -1), (0, 1),    # Torre
+            (-1, -1), (-1, 1), (1, -1), (1, 1)   # Bispo
+        ]
+        for dx, dy in direcoes:
+            i, j = x + dx, y + dy
+            while 0 <= i < 8 and 0 <= j < 8:
+                if tabuleiro[i][j] is None:
+                    movimentos.append((i, j))
+                elif tabuleiro[i][j].cor != self.cor:
+                    movimentos.append((i, j))
+                    break
+                else:
+                    break
+                i += dx
+                j += dy
+        return movimentos
